b2cb219e374c07a34b1abadbc59da239fc0e4f23
Đinh Ngọc Đức <22028166@vnu.edu.vn>
2025-05-13 22:18:31 +0700

    final

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,4 +1,4 @@
 library globals;
 
-const String openRouterApiKey = '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';
+const String openRouterApiKey = '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';
 String targetSearch = "";
